c687e164abff53d80b804ccbbbacd434a2021dc5
superkit <wangjiaolian1126@126.com>
2022-03-31 15:49:00 +0800

    chapter728

---
+++ chapter728.go
@@ -0,0 +1,21 @@
+package main
+
+func selfDividingNumbers(left int, right int) []int {
+	result := make([]int, 0)
+outer:
+	for i := left; i <= right; i++ {
+		temp := i
+		for temp > 0 {
+			if temp % 10==0{
+				continue outer
+			}
+			if i%(temp % 10) != 0 {
+				continue outer
+			}
+			temp = temp / 10
+		}
+		result = append(result, i)
+
+	}
+	return result
+}
