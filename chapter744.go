69fc3b05bb2088ecc5ea2a13b1a752f11b8b2d1a
superkit <wangjiaolian1126@126.com>
2022-04-03 20:26:09 +0800

    chapter744

---
+++ chapter744.go
@@ -0,0 +1,19 @@
+package main
+
+func nextGreatestLetter(letters []byte, target byte) byte {
+
+	left := 0
+	right := len(letters)
+
+	for left <= right {
+		mid := (left + right) / 2
+
+		if letters[mid] <= target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return letters[left]
+
+}
