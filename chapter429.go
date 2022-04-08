6fee7fa25b63549050743e90f13ae249f2d86e8e
superkit <wangjiaolian1126@126.com>
2022-04-08 15:29:45 +0800

    chapter429

---
+++ chapter429.go
@@ -0,0 +1,31 @@
+package main
+
+type Node struct {
+	Val      int
+	Children []*Node
+}
+
+func levelOrder(root *Node) [][]int {
+
+	if root == nil {
+		return [][]int{}
+	}
+
+	result := make([][]int, 0)
+
+	queue := make([]*Node, 0)
+	queue = append(queue, root)
+
+	for len(queue) > 0 {
+		temp := make([]int, 0)
+		tempQueue := make([]*Node, 0)
+		for i := 0; i < len(queue); i++ {
+			temp = append(temp, queue[i].Val)
+			tempQueue = append(tempQueue, queue[i].Children...)
+		}
+		result = append(result, temp)
+		queue = tempQueue
+	}
+	return result
+
+}
--- testSort.go
+++ testSort.go
@@ -1,12 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"leecode-go/sort"
-)
-
-func main() {
-	nums := []int{5, 7, 3, 6, 2}
-	sort.QuickSort(nums, 0, 4)
-	fmt.Printf("%v \n", nums)
-}
+//func main() {
+//	nums := []int{5, 7, 3, 6, 2}
+//	sort.QuickSort(nums, 0, 4)
+//	fmt.Printf("%v \n", nums)
+//}
