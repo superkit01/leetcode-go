7013a9ecf457a21b1d3a4118158793201da77ea9
superkit <wangjiaolian1126@126.com>
2022-04-12 12:12:46 +0800

    chapter141 806

---
+++ chapter141.go
@@ -0,0 +1,15 @@
+package main
+
+func hasCycle(head *ListNode) bool {
+	cache := make(map[*ListNode]int)
+	for head != nil {
+		if _, ok := cache[head]; ok {
+			return true
+		} else {
+			cache[head] = 1
+			head = head.Next
+		}
+	}
+	return false
+
+}
---
+++ chapter806.go
@@ -0,0 +1,16 @@
+package main
+
+func numberOfLines(widths []int, s string) []int {
+	total := 0
+	line := 1
+	for _, c := range s {
+		index := c - 'a'
+		if total+widths[index] > 100 {
+			total = widths[index]
+			line++
+		} else {
+			total += widths[index]
+		}
+	}
+	return []int{line, total}
+}
