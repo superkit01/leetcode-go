96a64f2c0b172a28d840739a7e612a73f7b332a1
Gogs <gogs@fake.local>
2022-07-15 11:55:13 +0800

    chapter203

---
+++ chapter203.go
@@ -0,0 +1,27 @@
+package main
+
+func removeElements(head *ListNode, val int) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	iter := head.Next
+	prev := head
+	for iter != nil {
+		if iter.Val != val {
+			prev = iter
+			iter = iter.Next
+		} else {
+			iter = iter.Next
+			prev.Next = iter
+		}
+
+	}
+
+	if head.Val == val {
+		return head.Next
+	}
+
+	return head
+
+}
