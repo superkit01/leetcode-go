6ec6d73d12050429718bd8c038c1d0a4f4c5d32e
Gogs <gogs@fake.local>
2022-07-19 09:24:32 +0800

    chapter2210

---
+++ chapter2210.go
@@ -0,0 +1,29 @@
+package main
+
+func countHillValley(nums []int) int {
+	temp := make([]int, 0)
+	temp = append(temp, nums[0])
+
+	for i := 1; i < len(nums); i++ {
+		if nums[i] == nums[i-1] {
+			continue
+		} else {
+			temp = append(temp, nums[i])
+		}
+	}
+
+	if len(temp) < 3 {
+		return 0
+	}
+	count := 0
+
+	for i := 1; i < len(temp)-1; i++ {
+		if temp[i] > temp[i-1] && temp[i] > temp[i+1] {
+			count += 1
+		}
+		if temp[i] < temp[i-1] && temp[i] < temp[i+1] {
+			count += 1
+		}
+	}
+	return count
+}
