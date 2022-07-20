126c54079dfd2eff64ecad48c12c171e10180dbb
Gogs <gogs@fake.local>
2022-07-20 15:18:58 +0800

    chapter228

---
+++ chapter228.go
@@ -0,0 +1,39 @@
+package main
+
+import "strconv"
+
+func summaryRanges(nums []int) []string {
+	if len(nums) == 0 {
+		return []string{}
+	}
+	if len(nums) == 1 {
+		return []string{strconv.Itoa(nums[0])}
+	}
+
+	result := make([]string, 0)
+
+	start := nums[0]
+	end := nums[0]
+
+	for i := 1; i < len(nums); i++ {
+		if nums[i]-end <= 1 {
+			end = nums[i]
+		} else {
+			if start == end {
+				result = append(result, strconv.Itoa(start))
+			} else {
+				result = append(result, strconv.Itoa(start)+"->"+strconv.Itoa(end))
+			}
+			start = nums[i]
+			end = nums[i]
+		}
+	}
+
+	if start == end {
+		result = append(result, strconv.Itoa(start))
+	} else {
+		result = append(result, strconv.Itoa(start)+"->"+strconv.Itoa(end))
+	}
+
+	return result
+}
