151989afd31555adfd193ee0195cd40ee62096df
Gogs <gogs@fake.local>
2022-07-20 09:23:51 +0800

    chapter237

---
+++ chapter237.go
@@ -0,0 +1,15 @@
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteNode(node *ListNode) {
+	//如何让自己在世界上消失，但又不死？ —— 将自己完全变成另一个人，再杀了那个人就行了。
+	node.Val = node.Next.Val
+	node.Next = node.Next.Next
+
+}
