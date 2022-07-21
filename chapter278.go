d625287787294c85b82cf908f0c3378d4e03d59a
Gogs <gogs@fake.local>
2022-07-21 15:04:31 +0800

    chapter278.og

---
+++ chapter278.go
@@ -0,0 +1,25 @@
+package main
+
+/**
+ * Forward declaration of isBadVersion API.
+ * @param   version   your guess about first bad version
+ * @return 	 	      true if current version is bad
+ *			          false if current version is good
+ *
+ */
+func isBadVersion(version int) bool
+
+func firstBadVersion(n int) int {
+	l := 1
+	r := n
+	for l <= r {
+		m := (l + r) / 2
+		if isBadVersion(m) {
+			r = m - 1
+		} else {
+			l = m + 1
+		}
+	}
+	return r + 1
+
+}
