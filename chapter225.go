585c7dfc0bd411c2523a96056d3cd2a4b8fe695e
Gogs <gogs@fake.local>
2022-07-21 14:35:27 +0800

    chapter232 chapter225

---
+++ chapter225.go
@@ -0,0 +1,49 @@
+package main
+
+type MyStack struct {
+	queue1 []int
+	queue2 []int
+}
+
+func Constructor() MyStack {
+	return MyStack{
+		queue1: make([]int, 0),
+		queue2: make([]int, 0),
+	}
+}
+
+func (this *MyStack) Push(x int) {
+	this.queue2 = append(this.queue2, x)
+	for len(this.queue1) > 0 {
+		this.queue2 = append(this.queue2, this.queue1[0])
+		this.queue1 = this.queue1[1:]
+	}
+
+	this.queue1, this.queue2 = this.queue2, this.queue1
+
+}
+
+func (this *MyStack) Pop() int {
+	v := this.queue1[0]
+	this.queue1 = this.queue1[1:]
+	return v
+}
+
+func (this *MyStack) Top() int {
+	return this.queue1[0]
+
+}
+
+func (this *MyStack) Empty() bool {
+	return len(this.queue1) == 0
+
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * param_2 := obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.Empty();
+ */
---
+++ chapter232.go
@@ -0,0 +1,46 @@
+package main
+
+type MyQueue struct {
+	stack1 []int
+	stack2 []int
+}
+
+func Constructor2() MyQueue {
+	return MyQueue{
+		stack1: make([]int, 0),
+		stack2: make([]int, 0),
+	}
+
+}
+
+func (this *MyQueue) Push(x int) {
+	for len(this.stack1) != 0 {
+		v := this.stack1[len(this.stack1)-1]
+		this.stack1 = this.stack1[0 : len(this.stack1)-1]
+		this.stack2 = append(this.stack2, v)
+	}
+
+	this.stack1 = append(this.stack1, x)
+
+	for len(this.stack2) != 0 {
+		v := this.stack2[len(this.stack2)-1]
+		this.stack2 = this.stack2[0 : len(this.stack2)-1]
+		this.stack1 = append(this.stack1, v)
+	}
+}
+
+func (this *MyQueue) Pop() int {
+	v := this.stack1[len(this.stack1)-1]
+	this.stack1 = this.stack1[0 : len(this.stack1)-1]
+	return v
+
+}
+
+func (this *MyQueue) Peek() int {
+	return this.stack1[len(this.stack1)-1]
+
+}
+
+func (this *MyQueue) Empty() bool {
+	return len(this.stack1) == 0
+}
