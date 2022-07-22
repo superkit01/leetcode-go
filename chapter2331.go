f2870d9d640ad5ca00a48e4083aba84f352b4965
Gogs <gogs@fake.local>
2022-07-22 14:31:38 +0800

    chapter2331

---
+++ chapter2331.go
@@ -0,0 +1,25 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func evaluateTree(root *TreeNode) bool {
+	if root.Val != 0 && root.Val != 1 {
+		if root.Val == 2 {
+			return evaluateTree(root.Left) || evaluateTree(root.Right)
+		} else {
+			return evaluateTree(root.Left) && evaluateTree(root.Right)
+		}
+	} else {
+		if root.Val == 0 {
+			return false
+		} else {
+			return true
+		}
+	}
+}
