058852d13d437e0e13922d96370ac60a3a95097d
Gogs <gogs@fake.local>
2022-07-22 15:06:09 +0800

    chapter2319

---
+++ chapter2319.go
@@ -0,0 +1,18 @@
+package main
+
+func checkXMatrix(grid [][]int) bool {
+	m := len(grid)
+	for i, v := range grid {
+		for j, w := range v {
+
+			if (i == j || i+j == m-1) && w == 0 {
+				return false
+			}
+
+			if (i != j && i+j != m-1) && w != 0 {
+				return false
+			}
+		}
+	}
+	return true
+}
