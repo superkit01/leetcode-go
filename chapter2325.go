2c17460a086dcc7a43f412c8a496170e243389c7
Gogs <gogs@fake.local>
2022-07-22 14:57:50 +0800

    chapter2325 2335

---
+++ chapter2325.go
@@ -0,0 +1,30 @@
+package main
+
+import "bytes"
+
+func decodeMessage(key string, message string) string {
+	secret := make(map[rune]rune)
+	temp := 'a'
+	for _, v := range key {
+		if v == ' ' {
+			continue
+		}
+
+		if _, ok := secret[v]; !ok {
+			secret[v] = temp
+			temp++
+		}
+
+	}
+	result := bytes.NewBufferString("")
+	for _, v := range message {
+		if v == ' ' {
+			result.WriteString(" ")
+		} else {
+			result.WriteRune(secret[v])
+		}
+
+	}
+	return result.String()
+
+}
---
+++ chapter2335.go
@@ -0,0 +1,16 @@
+package main
+
+import "sort"
+
+func fillCups(amount []int) int {
+	sort.Ints(amount)
+
+	sum := amount[0] + amount[1] + amount[2]
+
+	if amount[0]+amount[1] >= amount[2] {
+		return (sum + 1) / 2
+	} else {
+		return amount[2]
+	}
+
+}
