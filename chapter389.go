1586c4f646478f34557994e0a6a5537e17210837
Gogs <gogs@fake.local>
2022-07-25 13:58:24 +0800

    chapter389

---
+++ chapter389.go
@@ -0,0 +1,13 @@
+package main
+
+func findTheDifference(s string, t string) byte {
+	var result rune
+	for _, v := range s {
+		result ^= v
+	}
+	for _, v := range t {
+		result ^= v
+	}
+	return byte(result)
+
+}
