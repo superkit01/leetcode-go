b266247f5c048867ac2b7f49a337ee5b5c099b6f
Gogs <gogs@fake.local>
2022-07-25 13:53:54 +0800

    chapter383

---
+++ chapter383.go
@@ -0,0 +1,23 @@
+package main
+
+func canConstruct(ransomNote string, magazine string) bool {
+
+	cache := make(map[rune]int)
+
+	for _, v := range magazine {
+		if _, ok := cache[v]; !ok {
+			cache[v] = 0
+		}
+		cache[v]++
+	}
+
+	for _, v := range ransomNote {
+		if _, ok := cache[v]; !ok || cache[v] <= 0 {
+			return false
+		} else {
+			cache[v]--
+		}
+	}
+	return true
+
+}
