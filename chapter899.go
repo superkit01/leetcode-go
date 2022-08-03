746c2cef14022140b91c1911f41866f1008e242b
Gogs <gogs@fake.local>
2022-08-03 14:42:53 +0800

    chapter899

---
+++ chapter899.go
@@ -0,0 +1,27 @@
+package main
+
+import "sort"
+
+func orderlyQueue(s string, k int) string {
+
+	if k > 1 {
+		temp := []byte(s)
+		sort.Slice(temp,
+			func(i, j int) bool {
+				return temp[i] < temp[j]
+			})
+		return string(temp)
+	} else {
+		min := s
+		temp := s
+		for i := 0; i < len(s); i++ {
+			temp = temp[1:] + temp[:1]
+			if temp < min {
+				min = temp
+			}
+		}
+
+		return min
+	}
+
+}
