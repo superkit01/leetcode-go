80a1000e2b125ae44ac63e3a0b9648a0d0f47112
Gogs <gogs@fake.local>
2022-08-09 10:15:55 +0800

    chapter1413

---
+++ chapter1413.go
@@ -0,0 +1,25 @@
+package main
+
+func minStartValue(nums []int) int {
+	temp := 0
+
+	prefixSum := make([]int, len(nums))
+
+	for i := 0; i < len(nums); i++ {
+		temp += nums[i]
+		prefixSum[i] = temp
+	}
+
+	minValue := prefixSum[0]
+
+	for i := 0; i < len(prefixSum); i++ {
+		if minValue > prefixSum[i] {
+			minValue = prefixSum[i]
+		}
+	}
+	if minValue >= 1 {
+		return 1
+	} else {
+		return 1 - minValue
+	}
+}
