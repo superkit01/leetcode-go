802508c0d49723fc9def3a1a23376df9922f4bbb
Gogs <gogs@fake.local>
2022-08-18 08:52:46 +0800

    chapter404

---
+++ chapter404.go
@@ -0,0 +1,34 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func sumOfLeftLeaves(root *TreeNode) int {
+	cache := make([]int, 0)
+	sumOfLeftLeaves1(root, &cache, false)
+	sum := 0
+	for _, v := range cache {
+		sum += v
+	}
+	return sum
+
+}
+
+func sumOfLeftLeaves1(node *TreeNode, cache *[]int, isLeft bool) {
+	if node.Left == nil && node.Right == nil && isLeft {
+		*cache = append(*cache, node.Val)
+	}
+
+	if node.Left != nil {
+		sumOfLeftLeaves1(node.Left, cache, true)
+	}
+	if node.Right != nil {
+		sumOfLeftLeaves1(node.Right, cache, false)
+	}
+
+}
