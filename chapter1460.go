81a56e70d2d831bbb836272f0468eff2c0d0588a
Gogs <gogs@fake.local>
2022-08-24 16:45:39 +0800

    chapter1460

---
+++ chapter1460.go
@@ -0,0 +1,17 @@
+package main
+
+func canBeEqual(target []int, arr []int) bool {
+	cache := make(map[int]int, 0)
+	for i, _ := range target {
+		cache[target[i]]++
+		cache[arr[i]]--
+	}
+
+	for _, v := range cache {
+		if v != 0 {
+			return false
+		}
+	}
+	return true
+
+}
