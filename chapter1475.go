abf25b265a4dea5e6b24b28c33f87e2d75a91d8e
Gogs <gogs@fake.local>
2022-09-01 15:46:59 +0800

    fix

---
+++ chapter1475.go
@@ -0,0 +1,20 @@
+package main
+
+func finalPrices(prices []int) []int {
+	if len(prices) == 1 {
+		return prices
+	}
+	result := make([]int, 0)
+outer:
+	for i := 0; i < len(prices)-1; i++ {
+		for j := i + 1; j < len(prices); j++ {
+			if prices[j] <= prices[i] {
+				result = append(result, prices[i]-prices[j])
+				continue outer
+			}
+		}
+		result = append(result, prices[i])
+	}
+	result = append(result, prices[len(prices)-1])
+	return result
+}
