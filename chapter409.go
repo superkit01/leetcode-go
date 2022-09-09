3c7364a7b4998f3c8c560b986fe2ca1ab555d332
Gogs <gogs@fake.local>
2022-09-09 16:11:51 +0800

    chapter409

---
+++ chapter409.go
@@ -0,0 +1,24 @@
+package main
+
+func longestPalindrome(s string) int {
+	cache := make(map[rune]int, 0)
+	for _, v := range s {
+		cache[v]++
+	}
+
+	result := 0
+	flag := false
+	for _, v := range cache {
+		if v%2 == 0 {
+			result += v
+		} else {
+			result += (v - 1)
+			flag = true
+		}
+	}
+	if flag {
+		result += 1
+	}
+	return result
+
+}
