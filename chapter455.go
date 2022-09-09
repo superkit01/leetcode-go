4611af4613c333890ba035444f4f2c2551abb4ed
Gogs <gogs@fake.local>
2022-09-09 18:11:18 +0800

    chapter455

---
+++ chapter455.go
@@ -0,0 +1,26 @@
+package main
+
+import "sort"
+
+func findContentChildren(g []int, s []int) int {
+	if len(s) == 0 {
+		return 0
+	}
+	sort.Ints(g)
+	sort.Ints(s)
+
+	i := 0
+	j := 0
+	result := 0
+	for i < len(g) && j < len(s) {
+		if g[i] <= s[j] {
+			result++
+			i++
+			j++
+		} else {
+			j++
+		}
+	}
+	return result
+
+}
