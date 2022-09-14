3b16fc84e93f33c8ebb2e234357c7bf6a0d39073
Gogs <gogs@fake.local>
2022-09-14 17:48:07 +0800

    chapter1619

---
+++ chapter1619.go
@@ -0,0 +1,14 @@
+package main
+
+import "sort"
+
+func trimMean(arr []int) float64 {
+	sort.Ints(arr)
+	arr = arr[len(arr)/20 : len(arr)*19/20]
+	sum := 0
+	for _, v := range arr {
+		sum = sum + v
+	}
+	return float64(sum) / float64(len(arr))
+
+}
