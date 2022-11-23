7397edc089afd3a8e4df68f5628b32a75baf89ef
wangjiaolian <baron.wang@hi-dr.com>
2022-11-23 18:16:59 +0800

    chapter1742

---
+++ chapter1742.go
@@ -0,0 +1,29 @@
+package main
+
+func countBalls(lowLimit int, highLimit int) int {
+	cache := make(map[int]int)
+	for i := lowLimit; i <= highLimit; i++ {
+		sum := 0
+		v := i
+		for v != 0 {
+			sum = sum + v%10
+			v = v / 10
+		}
+
+		cache[sum]++
+	}
+
+	max := 0
+	for _, v := range cache {
+		if max < v {
+			max = v
+		}
+	}
+	return max
+}
+
+func main() {
+
+	countBalls(1, 10)
+
+}
