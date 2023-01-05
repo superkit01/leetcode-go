4977ddd7a62a7fbf6ae590867e729f1261ebbbe8
wangjiaolian <baron.wang@hi-dr.com>
2023-01-05 10:19:25 +0800

    turing2

--- week/turing2.go
+++ week/turing2.go
@@ -1,8 +1,27 @@
 package week
 
+func generateParenthesis(n int) []string {
+	result := make([]string, 0)
 
+	digui("", n, n, &result)
 
-func  generateParenthesis(n int )[]string{
+	return result
+}
 
+func digui(str string, left int, right int, result *[]string) {
+	if left == 0 && right == 0 {
+		*result = append(*result, str)
+		return
+	}
+
+	if left == right {
+		digui(str+"(", left-1, right, result)
+	} else if left < right {
+		if left > 0 {
+			digui(str+"(", left-1, right, result)
+		}
+		digui(str+")", left, right-1, result)
+
+	}
 
 }
