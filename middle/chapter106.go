3b2d1cfd3d477c1f89e7225cdbe3d9b5bf862675
wangjiaolian <baron.wang@hi-dr.com>
2023-01-12 16:28:59 +0800

    fix 106

--- middle/chapter106.go
+++ middle/chapter106.go
@@ -9,33 +9,33 @@ package middle
  * }
  */
 func buildTree2(inorder []int, postorder []int) *TreeNode {
-	return digui2(&inorder, postorder)
+	return digui2(inorder, &postorder)
 }
 
-func digui2(inorder *[]int, postorder []int) *TreeNode {
-	root := postorder[len(postorder)-1]
-	postorder = postorder[0 : len(postorder)-1]
+func digui2(inorder []int, postorder *[]int) *TreeNode {
+	root := (*postorder)[len(*postorder)-1]
+	*postorder = (*postorder)[0 : len(*postorder)-1]
 
 	tree := TreeNode{
 		Val: root,
 	}
 
 	index := -1
-	for i := 0; i < len(*inorder); i++ {
-		if (*inorder)[i] == root {
+	for i := 0; i < len(inorder); i++ {
+		if inorder[i] == root {
 			index = i
 			break
 		}
 	}
 
-	if index < len(*inorder)-1 {
-		right := (*inorder)[index+1:]
-		tree.Right = digui2(&right, postorder)
+	if index < len(inorder)-1 {
+		right := inorder[index+1:]
+		tree.Right = digui2(right, postorder)
 	}
 
 	if index > 0 {
-		left := (*inorder)[0:index]
-		tree.Left = digui2(&left, postorder)
+		left := inorder[0:index]
+		tree.Left = digui2(left, postorder)
 	}
 
 	return &tree
