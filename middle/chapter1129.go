dd57dd7504b7cea6adf7c1fded7c8b3ee972cdb7
wangjiaolian <baron.wang@hi-dr.com>
2023-02-02 16:28:47 +0800

    chapter1129

---
+++ middle/chapter1129.go
@@ -0,0 +1,85 @@
+package middle
+
+import "math"
+
+func shortestAlternatingPaths(n int, redEdges [][]int, blueEdges [][]int) []int {
+	redStart := make([]int, 0)
+	redStart = append(redStart, 0)
+	for i := 1; i < n; i++ {
+		redStart = append(redStart, findSteps(i, redEdges, blueEdges))
+	}
+
+	blueStart := make([]int, 0)
+	blueStart = append(blueStart, 0)
+	for i := 1; i < n; i++ {
+		blueStart = append(blueStart, findSteps(i, blueEdges, redEdges))
+	}
+
+	result := make([]int, 0)
+	result = append(result, 0)
+
+	for i := 1; i < n; i++ {
+		if redStart[i] == -1 {
+			result = append(result, blueStart[i])
+		} else if blueStart[i] == -1 {
+			result = append(result, redStart[i])
+		} else {
+			result = append(result, int(math.Min(float64(redStart[i]), float64(blueStart[i]))))
+		}
+	}
+
+	return result
+}
+
+func findSteps(n int, redEdges [][]int, blueEdges [][]int) int {
+
+	queue := make([]int, 0)
+	queue = append(queue, 0)
+
+	redCache := make(map[int]bool, 0)
+	redCache[0] = true
+
+	blueCache := make(map[int]bool, 0)
+
+	toggle := true
+	depth := 0
+	for len(queue) > 0 {
+		depth++
+		temp := make([]int, 0)
+		if toggle {
+			for _, v := range queue {
+				for _, red := range redEdges {
+					if red[0] == v {
+						if red[1] == n {
+							return depth
+						}
+						if _, ok := redCache[red[1]]; !ok {
+							temp = append(temp, red[1])
+							redCache[red[1]] = true
+						}
+					}
+				}
+			}
+		} else {
+			for _, v := range queue {
+				for _, blue := range blueEdges {
+					if blue[0] == v {
+						if blue[1] == n {
+							return depth
+						}
+						if _, ok := blueCache[blue[1]]; !ok {
+							temp = append(temp, blue[1])
+							blueCache[blue[1]] = true
+						}
+					}
+				}
+			}
+		}
+
+		toggle = !toggle
+		queue = temp
+	}
+
+	return -1
+
+}
