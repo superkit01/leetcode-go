12c129a838b4fe4ea845cce67a4ffed31556499f
wangjiaolian <baron.wang@hi-dr.com>
2023-02-03 17:49:27 +0800

    lcp08

---
+++ middle/lcp08.go
@@ -0,0 +1,47 @@
+package middle
+
+func getTriggerTime(increase [][]int, requirements [][]int) []int {
+	newIncrease := make([][]int, 0)
+	newIncrease = append(newIncrease, []int{0, 0, 0})
+	newIncrease = append(newIncrease, increase...)
+
+	for i := 1; i < len(newIncrease); i++ {
+		newIncrease[i][0] = newIncrease[i][0] + newIncrease[i-1][0]
+		newIncrease[i][1] = newIncrease[i][1] + newIncrease[i-1][1]
+		newIncrease[i][2] = newIncrease[i][2] + newIncrease[i-1][2]
+	}
+
+	result := make([]int, 0)
+
+	for _, v := range requirements {
+
+		i := search2(newIncrease, v)
+		result = append(result, i)
+	}
+
+	return result
+
+}
+
+func search2(increase [][]int, v []int) int {
+
+	if increase[len(increase)-1][0] < v[0] || increase[len(increase)-1][1] < v[1] || increase[len(increase)-1][2] < v[2] {
+		return -1
+	}
+
+	l := 0
+	r := len(increase) - 1
+
+	for l < r {
+		mid := (l + r) / 2
+
+		value := increase[mid]
+		if value[0] < v[0] || value[1] < v[1] || value[2] < v[2] {
+			l = mid + 1
+		} else {
+			r = mid
+		}
+	}
+
+	return l
+}
