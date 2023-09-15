772c683aba3c20c0cb7298e4534a0b897a873031
wangjiaolian <baron.wang@hi-dr.com>
2023-09-15 11:07:37 +0800

    lcp50

---
+++ easy/lcp50.go
@@ -0,0 +1,15 @@
+package easy
+
+import "sort"
+
+func giveGem(gem []int, operations [][]int) int {
+	for _, v := range operations {
+		transfer := gem[v[0]] / 2
+		gem[v[0]] = gem[v[0]] - transfer
+		gem[v[1]] = gem[v[1]] + transfer
+	}
+
+	sort.Ints(gem)
+	return gem[len(gem)-1] - gem[0]
+
+}
