d41b7f22b4fd6bdcdbf7385e71378ede44bf9bde
wangjiaolian <baron.wang@hi-dr.com>
2023-11-02 15:29:36 +0800

    fix

--- easy/chapter2103.go
+++ easy/chapter2103.go
@@ -1,36 +1,30 @@
 package easy
 
-import "golang.org/x/exp/slices"
-
 func CountPoints(rings string) int {
-	container := make(map[byte][]byte, 0)
+	container := make(map[byte]int, 0)
 
 	for i := 1; i < len(rings); i += 2 {
 		if _, ok := container[rings[i]]; !ok {
-			container[rings[i]] = make([]byte, 0)
+			container[rings[i]] = 0
 		}
-		container[rings[i]] = append(container[rings[i]], rings[i-1])
+		if rings[i-1] == 'B' {
+			container[rings[i]] = container[rings[i]] | 0b100
+		}
+		if rings[i-1] == 'G' {
+			container[rings[i]] = container[rings[i]] | 0b010
+		}
+		if rings[i-1] == 'R' {
+			container[rings[i]] = container[rings[i]] | 0b001
+		}
+
 	}
 
 	result := 0
 
 	for _, v := range container {
-		if len(v) < 3 {
-			continue
+		if v&0b111 == 0b111 {
+			result++
 		}
-
-		if !slices.Contains(v, 'B') {
-			continue
-		}
-
-		if !slices.Contains(v, 'G') {
-			continue
-		}
-
-		if !slices.Contains(v, 'R') {
-			continue
-		}
-		result++
 	}
 
 	return result
