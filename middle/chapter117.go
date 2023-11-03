0950a2b743a7cd4cf4147adc15a60db9fb465b92
wangjiaolian <baron.wang@hi-dr.com>
2023-11-03 10:58:50 +0800

    fix

--- middle/chapter117.go
+++ middle/chapter117.go
@@ -31,3 +31,36 @@ func connect1(root *Node1) *Node1 {
 	return root
 
 }
+
+func connect2(root *Node1) *Node1 {
+	if root == nil || (root.Left == nil && root.Right == nil) {
+		return root
+	}
+
+	queue := make([]*Node1, 0)
+	queue = append(queue, root)
+
+	for len(queue) > 0 {
+		tempQueue := make([]*Node1, 0)
+		for _, v := range queue {
+			if v.Left != nil {
+				tempQueue = append(tempQueue, v.Left)
+			}
+			if v.Right != nil {
+				tempQueue = append(tempQueue, v.Right)
+			}
+		}
+
+		if len(tempQueue) > 1 {
+			for i := 0; i < len(tempQueue)-1; i++ {
+				if tempQueue[i] != nil {
+					tempQueue[i].Next = tempQueue[i+1]
+				}
+			}
+		}
+		queue = tempQueue
+	}
+
+	return root
+
+}
