31ee3ab4178fa75d3e66b519f00d0844056ab37c
wangjiaolian <baron.wang@hi-dr.com>
2023-11-20 14:06:30 +0800

    chapter53-fix

--- middle/chapter53.go
+++ middle/chapter53.go
@@ -1,5 +1,7 @@
 package middle
 
+import "math"
+
 func maxSubArray(nums []int) int {
 
 	max := nums[0]
@@ -14,4 +16,19 @@ func maxSubArray(nums []int) int {
 	}
 	return max
 
+}
+
+func maxSubArray1(nums []int) int {
+
+	max := nums[0]
+
+	for i := 1; i < len(nums); i++ {
+		if nums[i]+nums[i-1] > nums[i] {
+			nums[i] = nums[i-1] + nums[i]
+		}
+		max = int(math.Max(float64(nums[i]), float64(max)))
+
+	}
+
+	return max
 }
