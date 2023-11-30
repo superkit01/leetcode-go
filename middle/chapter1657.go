d2534846b749d7671a86eec9d656cc938ea7b32a
wangjiaolian <baron.wang@hi-dr.com>
2023-11-30 18:55:09 +0800

    chapter1657

--- middle/chapter1657.go
+++ middle/chapter1657.go
@@ -1,5 +1,7 @@
 package middle
 
+import "sort"
+
 func closeStrings(word1 string, word2 string) bool {
 	if len(word1) != len(word2) {
 		return false
@@ -39,6 +41,50 @@ func closeStrings(word1 string, word2 string) bool {
 			return false
 		}
 
+	}
+
+	return true
+}
+
+func closeStrings1(word1 string, word2 string) bool {
+	if len(word1) != len(word2) {
+		return false
+	}
+
+	word1Map := make(map[rune]int, 0)
+	for _, v := range word1 {
+		word1Map[v]++
+	}
+
+	word2Map := make(map[rune]int, 0)
+	for _, v := range word2 {
+		word2Map[v]++
+	}
+
+	word1Arr := make([]int, 0)
+	for k, v := range word1Map {
+		if _, ok := word2Map[k]; !ok {
+			return false
+		}
+		word1Arr = append(word1Arr, v)
+	}
+
+	word2Arr := make([]int, 0)
+	for _, v := range word2Map {
+		word2Arr = append(word2Arr, v)
+	}
+
+	if len(word1Arr) != len(word2Arr) {
+		return false
+	}
+
+	sort.Ints(word1Arr)
+	sort.Ints(word2Arr)
+
+	for i := 0; i < len(word1Arr); i++ {
+		if word1Arr[i] != word2Arr[i] {
+			return false
+		}
 	}
 
 	return true
