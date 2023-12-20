e32449ef7ca48a3414d8e7cb7aa5c4d6b95f2c81
superkit01 <wangjiaolian1126@126.com>
2023-12-20 12:12:26 +0800

    chapter2828

---
+++ easy/chapter2828.go
@@ -0,0 +1,17 @@
+package easy
+
+func isAcronym(words []string, s string) bool {
+
+	if len(words) != len(s) {
+		return false
+	}
+
+	for i := 0; i < len(words); i++ {
+		if words[i][0] != s[i] {
+			return false
+		}
+
+	}
+	return true
+
+}
