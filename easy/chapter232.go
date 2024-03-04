22e105d4053c904015c633f36143419611beb4af
superkit01 <wangjiaolian1126@126.com>
2024-03-04 14:26:39 +0800

    optimize chapter232

--- easy/chapter232.go
+++ easy/chapter232.go
@@ -13,23 +13,51 @@ func Constructor2() MyQueue {
 
 }
 
+// func (this *MyQueue) Push(x int) {
+// 	for len(this.stack1) != 0 {
+// 		v := this.stack1[len(this.stack1)-1]
+// 		this.stack1 = this.stack1[0 : len(this.stack1)-1]
+// 		this.stack2 = append(this.stack2, v)
+// 	}
+
+// 	this.stack1 = append(this.stack1, x)
+
+// 	for len(this.stack2) != 0 {
+// 		v := this.stack2[len(this.stack2)-1]
+// 		this.stack2 = this.stack2[0 : len(this.stack2)-1]
+// 		this.stack1 = append(this.stack1, v)
+// 	}
+// }
+
+// func (this *MyQueue) Pop() int {
+// 	v := this.stack1[len(this.stack1)-1]
+// 	this.stack1 = this.stack1[0 : len(this.stack1)-1]
+// 	return v
+
+// }
+
+// func (this *MyQueue) Peek() int {
+// 	return this.stack1[len(this.stack1)-1]
+
+// }
+
+// func (this *MyQueue) Empty() bool {
+// 	return len(this.stack1) == 0
+// }
+
 func (this *MyQueue) Push(x int) {
-	for len(this.stack1) != 0 {
-		v := this.stack1[len(this.stack1)-1]
-		this.stack1 = this.stack1[0 : len(this.stack1)-1]
-		this.stack2 = append(this.stack2, v)
-	}
-
-	this.stack1 = append(this.stack1, x)
-
-	for len(this.stack2) != 0 {
-		v := this.stack2[len(this.stack2)-1]
-		this.stack2 = this.stack2[0 : len(this.stack2)-1]
-		this.stack1 = append(this.stack1, v)
-	}
+	this.stack2 = append(this.stack2, x)
 }
 
 func (this *MyQueue) Pop() int {
+	if len(this.stack1) == 0 {
+		for len(this.stack2) != 0 {
+			v := this.stack2[len(this.stack2)-1]
+			this.stack2 = this.stack2[0 : len(this.stack2)-1]
+			this.stack1 = append(this.stack1, v)
+		}
+	}
+
 	v := this.stack1[len(this.stack1)-1]
 	this.stack1 = this.stack1[0 : len(this.stack1)-1]
 	return v
@@ -37,10 +65,16 @@ func (this *MyQueue) Pop() int {
 }
 
 func (this *MyQueue) Peek() int {
+	if len(this.stack1) == 0 {
+		for len(this.stack2) != 0 {
+			v := this.stack2[len(this.stack2)-1]
+			this.stack2 = this.stack2[0 : len(this.stack2)-1]
+			this.stack1 = append(this.stack1, v)
+		}
+	}
 	return this.stack1[len(this.stack1)-1]
-
 }
 
 func (this *MyQueue) Empty() bool {
-	return len(this.stack1) == 0
+	return len(this.stack1) == 0 && len(this.stack2) == 0
 }
