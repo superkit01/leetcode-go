bcda7294fbfa353c491d452acd2e9ef0ef3191ee
superkit01 <wangjiaolian1126@126.com>
2024-03-07 16:00:03 +0800

    lcr121

---
+++ lcr/lcr121.go
@@ -0,0 +1,29 @@
+package lcr
+
+func findTargetIn2DPlants(plants [][]int, target int) bool {
+	if len(plants) == 0 || len(plants[0]) == 0 {
+		return false
+	}
+
+	m := 0
+	n := len(plants[0]) - 1
+
+	for m < len(plants) && n >= 0 {
+
+		if plants[m][n] < target {
+			m += 1
+			continue
+		}
+		if plants[m][n] > target {
+			n -= 1
+			continue
+		}
+
+		if plants[m][n] == target {
+			return true
+		}
+
+	}
+	return false
+
+}
