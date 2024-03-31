cac1e9427de81684f7e757e45ba245e0e8ef1e20
superkit01 <wangjiaolian1126@126.com>
2024-03-31 18:51:41 +0800

    rename

--- topics/quick_x.go
+++ topics/quick_x.go
@@ -1,7 +1,7 @@
 package topics
 
 //快速幂
-func quickMultiply(x, n int) int {
+func quickX(x, n int) int {
 	// n  1000101101
 
 	if n == 0 {
@@ -10,9 +10,9 @@ func quickMultiply(x, n int) int {
 
 	ans := 1
 	if n%2 == 1 {
-		ans = quickMultiply(x, n-1) * x
+		ans = quickX(x, n-1) * x
 	} else {
-		ans = quickMultiply(x, n>>1) * quickMultiply(x, n>>1)
+		ans = quickX(x, n>>1) * quickX(x, n>>1)
 	}
 	return ans
 
