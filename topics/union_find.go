ce8554f799987455420fb3f09131b007ce0b89c2
superkit01 <wangjiaolian1126@126.com>
2024-04-09 18:20:11 +0800

    union-find

--- topics/union_find.go
+++ topics/union_find.go
@@ -1,11 +1,35 @@
 package topics
 
-//并查集
-func find(x int) int {
-
-	return 0
+type UnionFind struct {
+	n      int
+	parent []int
 }
 
-func union(p, q int) {
+// 并查集
 
+func (uf UnionFind) Find(p int) int {
+	if p != uf.parent[p] {
+		uf.parent[p] = uf.Find(uf.parent[p])
+	}
+	return uf.parent[p]
+}
+
+func (uf UnionFind) Union(p, q int) {
+	pRoot := uf.Find(p)
+	qRoot := uf.Find(q)
+	if pRoot == qRoot {
+		return
+	}
+	uf.parent[pRoot] = qRoot
+}
+
+func New(n int) *UnionFind {
+	parent := make([]int, n)
+	for i := range parent {
+		parent[i] = i
+	}
+	return &UnionFind{
+		n:      n,
+		parent: parent,
+	}
 }
