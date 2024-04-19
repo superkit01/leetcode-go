38e6d8f6ac341b310edde85bfae4d5adc0ed663c
superkit01 <wangjiaolian1126@126.com>
2024-04-19 19:00:52 +0800

    topics

---
+++ topics/fast_slow.go
@@ -0,0 +1,14 @@
+package topics
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func hasCycle(head *ListNode) bool {
+
+}
+
+func middleNode(head *ListNode) *ListNode {
+
+}
---
+++ topics/lca.go
@@ -0,0 +1,20 @@
+package topics
+
+//lowest common ancesters
+//最近公共祖先
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+//dfs 深度优先 递归
+func lca(root, p, q *TreeNode) {
+
+}
+
+//带深度的中序遍历  |左|跟|右|
+// lca一定在数组的p、q节点对应位置的中间 且深度最低
+func lcaII(root, p, q *TreeNode) {
+
+}
