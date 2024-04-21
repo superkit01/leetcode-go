2d1acea40f0a9c9cb75359e790c15fa197ef1883
superkit01 <wangjiaolian1126@126.com>
2024-04-21 11:52:16 +0000

    chapter77

---
+++ middle/chapter77.go
@@ -0,0 +1,41 @@
+package middle
+
+func combine(n int, k int) [][]int {
+    ans:= make([][]int,0)
+    
+    dfs77(n,k,1,make([]int,0),&ans)
+    
+    return ans
+
+}
+
+
+func dfs77(n int, k int,index int, current []int, ans *[][]int){
+    if len(current)>k {
+        return
+    }
+    
+    if len(current)==k {
+        temp:=make([]int,0)
+        temp=append(temp,current...)
+        *ans=append(*ans,temp)
+        return
+    }
+    
+    if index>n {
+        return
+    }
+    
+    
+    //选择当前位置
+    temp2:=make([]int,0)
+    temp2=append(temp2,current...)
+    temp2=append(temp2,index)
+    dfs77(n,k,index+1, temp2,ans)
+    
+    //不选择当前位置
+    temp3:=make([]int,0)
+    temp3=append(temp3,current...)
+    dfs77(n,k,index+1, temp3,ans)
+    
+}
