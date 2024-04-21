3b0928ad16758eb4460f03e69c7ced43b36b2895
superkit01 <wangjiaolian1126@126.com>
2024-04-22 00:13:12 +0800

    nothing

--- week/week394/question1.go
+++ week/week394/question1.go
@@ -1,30 +1,28 @@
 package week394
 
-
-
 func numberOfSpecialChars(word string) int {
-    offset:=int('A')- int('a')
-    
-    cache:= make(map[int]bool,0)
-    for _,v:=range word {
-        if v>='A' && v <='Z'{
-             cache[int(v)]=true
-        }
-    }
-    
-    ans:=0
-    cache2:= make(map[int]bool,0)
-    for _,v:=range word {
-      if v>='a' && v<='z' {
-         lowerCase:=int(v)+offset
-         if _,ok:=cache[lowerCase];ok{
-            if _,ok:=cache2[lowerCase];!ok {
-               ans+=1
-               cache2[lowerCase]=true
-            }       
-         }
-      }
-    }
-   
-    return ans
+	offset := int('A') - int('a')
+
+	cache := make(map[int]bool, 0)
+	for _, v := range word {
+		if v >= 'A' && v <= 'Z' {
+			cache[int(v)] = true
+		}
+	}
+
+	ans := 0
+	cache2 := make(map[int]bool, 0)
+	for _, v := range word {
+		if v >= 'a' && v <= 'z' {
+			lowerCase := int(v) + offset
+			if _, ok := cache[lowerCase]; ok {
+				if _, ok := cache2[lowerCase]; !ok {
+					ans += 1
+					cache2[lowerCase] = true
+				}
+			}
+		}
+	}
+
+	return ans
 }
--- week/week394/question2.go
+++ week/week394/question2.go
@@ -1,36 +1,33 @@
 package week394
 
+func numberOfSpecialCharsII(word string) int {
+	offset := int('A') - int('a')
 
+	cache := make(map[int]int, 0)
 
-func numberOfSpecialChars(word string) int {
-    offset:=int('A')- int('a')
-    
-    cache:=make(map[int]int,0)
-    
-    for i,v:=range word {
-        if v>='a' && v<='z' {
-            cache[int(v)]=i
-        }
-    }
-    
-    
-    ans:=0
-    cache2:=make(map[int]bool,0)
-    
-    for i,v:=range word {
-     if v>='A' && v<='Z' {
-         lowerCase:=int(v)-offset
-         if v,ok:=cache[lowerCase];ok {
-            if v>=i {
-               cache2[lowerCase]=true
-            }else{
-              if _,ok:=cache2[lowerCase];!ok {  
-                ans+=1
-                cache2[lowerCase]=true
-              }  
-            }
-         }
-      }
-   }
-   return ans
+	for i, v := range word {
+		if v >= 'a' && v <= 'z' {
+			cache[int(v)] = i
+		}
+	}
+
+	ans := 0
+	cache2 := make(map[int]bool, 0)
+
+	for i, v := range word {
+		if v >= 'A' && v <= 'Z' {
+			lowerCase := int(v) - offset
+			if v, ok := cache[lowerCase]; ok {
+				if v >= i {
+					cache2[lowerCase] = true
+				} else {
+					if _, ok := cache2[lowerCase]; !ok {
+						ans += 1
+						cache2[lowerCase] = true
+					}
+				}
+			}
+		}
+	}
+	return ans
 }
