49866fc7b082302c6bd483ac06af603e05a08e48
superkit <wangjiaolian1126@126.com>
2024-04-21 10:29:46 +0800

    format

--- middle/chapter39.go
+++ middle/chapter39.go
@@ -1,42 +1,40 @@
 package middle
 
-
+// dfs   回溯 + 剪枝
 func combinationSum(candidates []int, target int) [][]int {
-    
-    ans:=make([][]int,0)
-    dfs39(candidates,0,target,[]int{},&ans)
-    return ans
+
+	ans := make([][]int, 0)
+	dfs39(candidates, 0, target, []int{}, &ans)
+	return ans
 
 }
 
 //dfs  i位置 使用或不使用
-func  dfs39(candidates []int,index int,remaining int,current []int,ans *[][]int){
-    if remaining <0 {
-        return 
-    }
-    
-    if remaining == 0{ 
-        temp3:=make([]int,0)
-        temp3=append(temp3,current...)
-        
-        *ans=append(*ans,temp3)
-        return
-    }
-    if index >= len(candidates){
-        return
-    }
-    
-    //使用index位置
-    temp:=make([]int,0)
-    temp=append(temp,current...)
-    temp=append(temp,candidates[index])
-    dfs39(candidates, index,remaining-candidates[index],temp,ans)
-    
-    //不使用index位置
-    temp2:=make([]int,0)
-    temp2=append(temp2,current...)
-    dfs39(candidates, index+1,remaining,temp2,ans)
-   
-    
-    
+func dfs39(candidates []int, index int, remaining int, current []int, ans *[][]int) {
+	if remaining < 0 {
+		return
+	}
+
+	if remaining == 0 {
+		temp3 := make([]int, 0)
+		temp3 = append(temp3, current...)
+
+		*ans = append(*ans, temp3)
+		return
+	}
+	if index >= len(candidates) {
+		return
+	}
+
+	//使用index位置
+	temp := make([]int, 0)
+	temp = append(temp, current...)
+	temp = append(temp, candidates[index])
+	dfs39(candidates, index, remaining-candidates[index], temp, ans)
+
+	//不使用index位置
+	temp2 := make([]int, 0)
+	temp2 = append(temp2, current...)
+	dfs39(candidates, index+1, remaining, temp2, ans)
+
 }
--- topics/fast_slow.go
+++ topics/fast_slow.go
@@ -5,22 +5,19 @@ type ListNode struct {
 	Next *ListNode
 }
 
-//判断链表是否有环
+// 判断链表是否有环
 func hasCycle(head *ListNode) bool {
-	
 
+	return false
 }
 
-
-//找出链表中环的进入点notig
+// 找出链表中环的进入点notig
 func findEntryOfCycle(head *ListNode) *ListNode {
-	
-	
-	
+
+	return nil
 }
 
-
-//链表的中间节点
+// 链表的中间节点
 func middleNode(head *ListNode) *ListNode {
-
+	return nil
 }
