dd8fb2b0d6fc99229095d03d56edbc14948f046a
superkit01 <wangjiaolian1126@126.com>
2024-04-22 18:37:50 +0800

    lcr132

--- lcr/lcr132.go
+++ lcr/lcr132.go
@@ -9,11 +9,11 @@ func cuttingBambooII(bamboo_len int) int {
 	}
 
 	if bamboo_len%3 == 1 {
-		return 4 * quickX(3, (bamboo_len-4)/3)
+		return 4 * quickX(3, (bamboo_len-4)/3) % 1000000007
 	} else if bamboo_len%3 == 2 {
-		return 2 * quickX(3, (bamboo_len-2)/3)
+		return 2 * quickX(3, (bamboo_len-2)/3) % 1000000007
 	} else {
-		return quickX(3, bamboo_len/3)
+		return quickX(3, bamboo_len/3) % 1000000007
 	}
 
 }
@@ -24,9 +24,9 @@ func quickX(x, n int) int {
 	}
 
 	if n%2 == 1 {
-		return quickX(x, n-1) * x % 1000000007
+		return x * quickX(x, n-1) % 1000000007
 	}
-
-	return quickX(x, n/2) * quickX(x, n/2) % 1000000007
+	temp := quickX(x, n/2)
+	return temp * temp % 1000000007
 
 }
