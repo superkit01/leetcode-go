292e392ab93296b075666b4042b41a7f36e7e5de
superkit01 <wangjiaolian1126@126.com>
2024-04-24 17:21:17 +0800

    lcr031

--- main.go
+++ main.go
@@ -150,5 +150,18 @@ func main() {
 	middle.MaxSatisfied([]int{10, 1, 7}, []int{0, 0, 0}, 2)
 
 	lcr.AddBinary("11", "10")
+	
 	lcr.SingleNumber([]int{-2})
+
+
+	cache:=lcr.ConstructorLRU(2)
+	cache.Put(1,1)
+	cache.Put(2,2)
+	cache.Get(1)
+	cache.Put(3,3)
+	cache.Get(2)
+	cache.Put(4,4)
+	cache.Get(1)
+	cache.Get(3)
+	cache.Get(4)
 }
