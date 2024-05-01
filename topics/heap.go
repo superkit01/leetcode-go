7355c2a453f2cb8fd9be40dcc153ca6c76b85006
superkit01 <wangjiaolian1126@126.com>
2024-05-01 19:25:54 +0800

    topic heap

---
+++ topics/heap.go
@@ -0,0 +1,31 @@
+package topics
+
+type S struct {
+	Key   int
+	Value int
+}
+
+type Heap []*S
+
+func (h Heap) Len() int {
+	return len(h)
+}
+
+func (h Heap) Less(i, j int) bool {
+	return h[i].Key < h[j].Key
+}
+
+func (h Heap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *Heap) Push(x interface{}) {
+	*h = append(*h, x.(*S))
+}
+
+func (h *Heap) Pop() interface{} {
+	old := *h
+	x := old[len(old)-1]
+	*h = old[0 : len(old)-1]
+	return x
+}
