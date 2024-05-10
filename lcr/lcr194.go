0a3ddc4098d5d0aed2ee3dfc4af332933b29c32b
superkit01 <wangjiaolian1126@126.com>
2024-05-10 19:02:50 +0800

    lcr194

---
+++ lcr/lcr194.go
@@ -0,0 +1,55 @@
+package lcr
+
+import "math"
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	inOrder := make([]*DeepNode, 0)
+
+	//带深度的中序遍历 |左|跟|右|
+	dfs(root, &inOrder, 1)
+
+	pIndex := -1
+	qIndex := -1
+	for i := 0; i < len(inOrder); i++ {
+		if inOrder[i].node == p {
+			pIndex = i
+		}
+		if inOrder[i].node == q {
+			qIndex = i
+		}
+	}
+
+	if pIndex > qIndex {
+		pIndex, qIndex = qIndex, pIndex
+	}
+
+	maxDeep := math.MaxInt32
+	var ans *TreeNode
+	for i := pIndex; i <= qIndex; i++ {
+		if maxDeep > inOrder[i].deep {
+			maxDeep = inOrder[i].deep
+			ans = inOrder[i].node
+		}
+	}
+
+	return ans
+
+}
+
+func dfs(root *TreeNode, inOrder *[]*DeepNode, deep int) {
+	if root == nil {
+		return
+	}
+	dfs(root.Left, inOrder, deep+1)
+	*inOrder = append(*inOrder, &DeepNode{
+		node: root,
+		deep: deep,
+	})
+	dfs(root.Right, inOrder, deep+1)
+
+}
+
+type DeepNode struct {
+	node *TreeNode
+	deep int
+}
