98ba24ba29cdd396cb247cf81d5038e297a84086
superkit01 <wangjiaolian1126@126.com>
2024-05-12 21:53:02 +0800

    chapter1553

---
+++ hard/chapter1553.go
@@ -0,0 +1,28 @@
+package hard
+
+func minDays(n int) int {
+	cache := make(map[int]int, 0)
+	return dfs(n, &cache)
+
+}
+
+func dfs(n int, cache *map[int]int) int {
+	if v, ok := (*cache)[n]; ok {
+		return v
+	}
+	if n <= 2 {
+		(*cache)[n] = n
+		return n
+	}
+	temp := min(dfs(n/2, cache)+1+n%2, dfs(n/3, cache)+1+n%3)
+	(*cache)[n] = temp
+	return temp
+
+}
+
+func min(i, j int) int {
+	if i < j {
+		return i
+	}
+	return j
+}
