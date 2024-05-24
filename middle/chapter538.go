79b109b43aba6b60041ec7a39c717897b3105f31
superkit01 <wangjiaolian1126@126.com>
2024-05-24 17:36:34 +0800

    chapter538

---
+++ middle/chapter538.go
@@ -0,0 +1,26 @@
+package middle
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func convertBST(root *TreeNode) *TreeNode {
+	sum := 0
+	var dfs func(root *TreeNode)
+	dfs = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+		dfs(root.Right)
+		sum += root.Val
+		root.Val = sum
+		dfs(root.Left)
+	}
+
+	dfs(root)
+	return root
+}
