68c7b560b414bdac363b88cc0c2c5c70c187fa62
superkit01 <wangjiaolian1126@126.com>
2024-05-24 17:18:36 +0800

    chapter543

---
+++ easy/chapter543.go
@@ -0,0 +1,37 @@
+package easy
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func diameterOfBinaryTree(root *TreeNode) int {
+	ans := 0
+
+	var dfs func(root *TreeNode) int
+
+	dfs = func(root *TreeNode) int {
+		if root == nil {
+			return 0
+		}
+		lDeep := dfs(root.Left)
+		rDeep := dfs(root.Right)
+		ans = max(ans, lDeep+rDeep)
+
+		return max(lDeep, rDeep) + 1
+	}
+
+	dfs(root)
+
+	return ans
+}
+
+func max(i, j int) int {
+	if i < j {
+		return j
+	}
+	return i
+}
