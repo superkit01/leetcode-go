d8dbd0f9ffd02fb82ac36003db1986a46ce720f5
superkit01 <wangjiaolian1126@126.com>
2024-06-09 00:33:59 +0800

    dweek132

--- dweek/dweek132/question3.go
+++ dweek/dweek132/question3.go
@@ -1,11 +1,13 @@
 package dweek132
 
+//TLE
 func MaximumLengthII(nums []int, k int) int {
 	maxV := 0
-	var dfs func(index int, preValue int, cnt int, length int)
 
-	dfs = func(index int, preValue int, cnt int, length int) {
-		if cnt > k {
+	var dfs func(index int, preValue int, diffCnt int, length int)
+
+	dfs = func(index int, preValue int, diffCnt int, length int) {
+		if diffCnt > k {
 			return
 		}
 		maxV = max(maxV, length)
@@ -14,34 +16,31 @@ func MaximumLengthII(nums []int, k int) int {
 			return
 		}
 
-		//选择当前位置
-		if preValue == -1 {
-			if nums[index] != preValue {
-				dfs(index+1, nums[index], cnt, length+1)
-			} else {
-				dfs(index+1, nums[index], cnt, length+1)
-			}
+		//选择index位置
+		if preValue == -1 { //初始值
+			dfs(index+1, nums[index], diffCnt, length+1)
 		} else {
 			if nums[index] != preValue {
-				dfs(index+1, nums[index], cnt+1, length+1)
+				dfs(index+1, nums[index], diffCnt+1, length+1)
 			} else {
-				dfs(index+1, nums[index], cnt, length+1)
+				dfs(index+1, nums[index], diffCnt, length+1)
 			}
-
 		}
 
-		//不选择当前位置
-		dfs(index+1, preValue, cnt, length)
+		//不选择index位置
+		dfs(index+1, preValue, diffCnt, length)
+
 	}
 
 	dfs(0, -1, 0, 0)
+
 	return maxV
 
 }
 
 func max(i, j int) int {
-	if i > j {
-		return i
+	if i < j {
+		return j
 	}
-	return j
+	return i
 }
