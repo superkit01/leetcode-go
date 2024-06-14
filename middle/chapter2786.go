19d052a8c09c1b5058a9d39f11eb2de16d658f04
superkit01 <wangjiaolian1126@126.com>
2024-06-14 15:45:10 +0800

    fix

---
+++ middle/chapter2786.go
@@ -0,0 +1,70 @@
+package middle
+
+import "math"
+
+func maxScore(nums []int, x int) int64 {
+	dp := make([]int64, len(nums))
+	even := -1
+	odd := -1
+	dp[0] = int64(nums[0])
+	if nums[0]%2 == 1 {
+		odd = 0
+	} else {
+		even = 0
+	}
+
+	for i := 1; i < len(nums); i++ {
+		if nums[i]%2 == nums[i-1]%2 {
+			dp[i] = dp[i-1] + int64(nums[i])
+			if nums[i]%2 == 1 {
+				odd = odd + 1
+			} else {
+				even = even + 1
+			}
+
+		} else {
+			if nums[i]%2 == 1 {
+				if nums[odd]%2 == nums[i]%2 {
+					dp[i] = maxInt64(dp[i-1]-int64(x), dp[odd]) + int64(nums[i])
+				} else {
+					dp[i] = dp[i-1] + int64(nums[i]) - int64(x)
+				}
+				odd = i
+			} else {
+				if nums[even]%2 == nums[i]%2 {
+					dp[i] = maxInt64(dp[i-1]-int64(x), dp[even]) + int64(nums[i])
+				} else {
+					dp[i] = dp[i-1] + int64(nums[i]) - int64(x)
+				}
+				even = i
+			}
+
+		}
+
+	}
+
+	return dp[len(dp)]
+}
+
+func maxInt64(i, j int64) int64 {
+	if i > j {
+		return i
+	}
+	return j
+}
+
+func maxScoreII(nums []int, x int) int64 {
+	dp := make([][2]int64, len(nums))
+	//[[偶][奇]]
+	dp[0] = [2]int64{math.MinInt32, math.MinInt32}
+	dp[0][nums[0]%2] = int64(nums[0])
+	for i := 1; i < len(nums); i++ {
+		dp[i] = [2]int64{dp[i-1][0], dp[i-1][1]}
+		if nums[i]%2 == 0 {
+			dp[i][0] = maxInt64(dp[i-1][0]+int64(nums[i]), dp[i-1][1]+int64(nums[i])-int64(x))
+		} else {
+			dp[i][1] = maxInt64(dp[i-1][1]+int64(nums[i]), dp[i-1][0]+int64(nums[i])-int64(x))
+		}
+	}
+	return maxInt64(dp[len(dp)-1][0], dp[len(dp)-1][1])
+}
